d25f63739d7c48037e34c3bd9318d47945a0200e
brazeenNP <s10255790@connect.np.edu.sg>
2024-05-22 18:31:40 +0800

    initial commit, no db functionality

---
+++ app/src/main/java/sg/edu/np/mad/travelhub/EditProfile.java
@@ -0,0 +1,37 @@
+package sg.edu.np.mad.travelhub;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.EPmain), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //edit profile button
+        Button doneButton = findViewById(R.id.doneButton);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backToSettings = new Intent(getApplicationContext(), Settings.class);
+                startActivity(backToSettings);
+            }
+        });
+    }
+}
