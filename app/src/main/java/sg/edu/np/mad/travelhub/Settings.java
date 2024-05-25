6f7391f2ff45209e98e81df93ad0a40b6eb13365
brazeenNP <s10255790@connect.np.edu.sg>
2024-05-25 17:36:52 +0800

    sharedPreferences should work, edit profile no functionality yet

--- app/src/main/java/sg/edu/np/mad/travelhub/Settings.java
+++ app/src/main/java/sg/edu/np/mad/travelhub/Settings.java
@@ -1,10 +1,13 @@
 package sg.edu.np.mad.travelhub;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -23,9 +26,27 @@ public class Settings extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Storing data into SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("settings",MODE_PRIVATE);
 
-        //edit profile button
+// Creating an Editor object to edit(write to the file)
+        boolean mpnState = sharedPreferences.getBoolean("mpn", false);
+        boolean asState = sharedPreferences.getBoolean("as", false);
+        boolean baState = sharedPreferences.getBoolean("ba", false);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
         Button epButton =findViewById(R.id.epButton);
+
+        SwitchMaterial mpnButton =findViewById(R.id.mpnButton);
+        SwitchMaterial baButton =findViewById(R.id.baButton);
+        SwitchMaterial asButton =findViewById(R.id.asButton);
+        mpnButton.setChecked(sharedPreferences.getBoolean("mpn", false));
+        asButton.setChecked(sharedPreferences.getBoolean("as", false));
+        baButton.setChecked(sharedPreferences.getBoolean("ba", false));
+
+        myEdit.putBoolean("mpn", mpnState);
+        myEdit.putBoolean("as", asState);
+        myEdit.putBoolean("ba", baState);
         epButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,5 +55,30 @@ public class Settings extends AppCompatActivity {
             }
         });
 
+        mpnButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                myEdit.putBoolean("mpn", isChecked);
+                myEdit.apply();
+            }
+        });
+
+        asButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                myEdit.putBoolean("as", isChecked);
+                myEdit.apply();
+            }
+        });
+
+        baButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                myEdit.putBoolean("ba", isChecked);
+                myEdit.apply();
+            }
+        });
+
+        myEdit.commit();
     }
 }
