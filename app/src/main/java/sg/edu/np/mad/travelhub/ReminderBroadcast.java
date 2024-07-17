9e240348aef1e778a37e202204700f24d5c98938
Rishikesh <148840508+RISHIKEsH12321@users.noreply.github.com>
2024-07-17 15:10:40 +0800

    feat: Added Intent to Open Up App in the Notification & Added Event Title To the Notification

--- app/src/main/java/sg/edu/np/mad/travelhub/ReminderBroadcast.java
+++ app/src/main/java/sg/edu/np/mad/travelhub/ReminderBroadcast.java
@@ -2,6 +2,7 @@ package sg.edu.np.mad.travelhub;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -20,6 +21,7 @@ public class ReminderBroadcast extends BroadcastReceiver {
     private static int reminderID = 0;
     private static final String channelID = "PlanHub";
     private static final String titleExtra = "titleExtra";
+    private static final String titleMain = "titleMain";
     private static final String messageExtra = "messageExtra";
 
     @Override
@@ -29,14 +31,25 @@ public class ReminderBroadcast extends BroadcastReceiver {
         Bitmap largeIconBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.planhub_icon);
 
 
+        // Create an intent that opens the ViewEvents activity
+        Intent viewEventsIntent = new Intent(context, ViewEvents.class);
+        // Ensure the activity is launched as a new task
+        viewEventsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+
+        PendingIntent calendarIntent = PendingIntent.getActivity(context, 0, viewEventsIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+
         Notification notification = new NotificationCompat.Builder(context, channelID)
                 .setLargeIcon(largeIconBitmap) // Set the large icon here
                 .setSmallIcon(R.drawable.planhub_icon)
-                .setContentTitle(intent.getStringExtra(titleExtra))
-                .setContentText(intent.getStringExtra(messageExtra))
+                .setContentTitle(intent.getStringExtra(titleMain))
+                .setContentText(intent.getStringExtra(titleExtra))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_REMINDER)
+                .setContentIntent(calendarIntent)
                 .build();
+
 
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 //        int reminderID = intent.getIntExtra("reminderID", 1);
