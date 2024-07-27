88339477f401382c1ce0e1d8ca6e697929555ac1
Rishikesh <148840508+RISHIKEsH12321@users.noreply.github.com>
2024-07-28 07:26:01 +0800

    bug: Fix loading post in profile and added theme to follower slider

--- app/src/main/java/sg/edu/np/mad/travelhub/Posts.java
+++ app/src/main/java/sg/edu/np/mad/travelhub/Posts.java
@@ -78,7 +78,7 @@ public class Posts extends Fragment {
 
         //list
         postMap = new HashMap<>();
-//        adapter = new ParentAdapter(getActivity().getApplicationContext());
+        adapter = new ParentAdapter(getActivity().getApplicationContext());
         recyclerView.setAdapter(adapter);
         loadingDialog.startLoadingDialog();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
@@ -107,8 +107,11 @@ public class Posts extends Fragment {
                                 filteredMap.put(entry.getKey(), parentItem);
                             }
                         }
-                        adapter.setParentItemMap(filteredMap);
-                        adapter.notifyDataSetChanged();
+                        if (filteredMap != null){
+                            adapter.setParentItemMap(filteredMap);
+                            adapter.notifyDataSetChanged();
+                        }
+
                     } else {
                         Log.e("UID", "FirebaseUser is null");
                     }
